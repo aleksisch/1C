4b79f2dfbc38437690bd2c4445154e4ea2539438
lekhachurkin@gmail.com <lekhachurkin@gmail.com>
2021-04-09 22:53:29 +0300

    small changes

--- src/Decryptor.cpp
+++ src/Decryptor.cpp
@@ -6,9 +6,8 @@ Decryptor::Decryptor(const std::string& filename, int max_key_size) : max_key_si
     std::ifstream f(filename);
     std::deque<std::string> words;
     word_number = 1;
-    while (f) {
-        std::string current_word;
-        f >> current_word;
+    std::string current_word;
+    while (f >> current_word) {
         words.emplace_back(current_word);
         if (words.size() == max_key_size_) {
             InsertWord(words);
@@ -20,14 +19,14 @@ Decryptor::Decryptor(const std::string& filename, int max_key_size) : max_key_si
 }
 
 void Decryptor::InsertWord(std::deque<std::string>& words) {
-    std::string res;
+    std::string insert_string;
     for (int i = 0; i < words.size(); i++) {
-        if (words[i].size() < i) {
+        if (words[i].size() <= i) {
             break;
         }
-        res += words[i][i];
+        insert_string += words[i][i];
     }
-    trie_.Add(res, word_number);
+    trie_.Add(insert_string, word_number);
     word_number++;
     words.pop_front();
 }
